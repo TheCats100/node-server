ed49009e26ac8b30ea485cdea1a23d131e91f10b
TheCats100 <61271886+TheCats100@users.noreply.github.com>
2020-05-28 12:11:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (request, response) => {
+    response.send('Welcome')
+})
+
+app.get('/api/movies', (request, response) => {
+  response.send('Récupération de tous les films')
+});
+
+app.get('/api/movies/:id', (request, response) => {
+    const { id } = request.params
+    response.json({ id })
+  });
+
+app.get('/api/employee', (request, response) => {
+    const  { name }  = request.query;
+    response.status(404).send(`Impossible de récupérer l'employé ${name}`);
+});
+
+app.get('/api/employees', (request, response) => {
+    //route légèrement modifiée pour éviter le conflit
+    response.sendStatus(304)
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
+
+  console.log(`Server is listening on ${port}`);
+});
