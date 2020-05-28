1b74a9ebc4b18ce6fd4e49cd7b2094b7f6874e33
TheCats100 <61271886+TheCats100@users.noreply.github.com>
2020-05-28 16:39:48 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-const connection = require('./conf');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 
-// écoute de l'url "/api/employees"
-app.get('/api/movies', (req, res) => {
-
-    // connection à la base de données, et sélection des employés
-    connection.query('SELECT * from movie', (err, results) => {
-  
-    if (err) {
-        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-        res.status(500).send('Erreur lors de la récupération des films');
-
-    } else {
-        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-        res.json(results);
-        }
-
-    });
-});
-
-app.get('/api/movies/names', (req, res) => {
-
-    connection.query('SELECT name from movie', (err, results) =>{
-
-        if(err) {
-            res.status(500).send('No body was here..')
-        } else {
-            res.json(results);
-        }
-    })
+app.get('/', (request, response) => {
+    response.send('Welcome')
 })
 
+app.get('/api/movies', (request, response) => {
+  response.send('Récupération de tous les films')
+});
 
-  
+app.get('/api/movies/:id', (request, response) => {
+    const { id } = request.params
+    response.json({ id })
+  });
+
+app.get('/api/employee', (request, response) => {
+    const  { name }  = request.query;
+    response.status(404).send(`Impossible de récupérer l'employé ${name}`);
+});
+
+app.get('/api/employees', (request, response) => {
+    //route légèrement modifiée pour éviter le conflit
+    response.sendStatus(304)
+});
+
 app.listen(port, (err) => {
-    if (err) {
-        throw new Error('Something bad happened...');
-}
+  if (err) {
+    throw new Error('Something bad happened...');
+  }
 
-    console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${port}`);
 });
