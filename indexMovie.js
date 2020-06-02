6031fcee52857b4c8230dc989e188307c33bfd71
TheCats100 <61271886+TheCats100@users.noreply.github.com>
2020-06-02 09:08:36 +0200

    Update indexMovie.js

--- indexMovie.js
+++ indexMovie.js
@@ -6,26 +6,20 @@ const port = 3000;
 
 // écoute de l'url "/api/employees"
 app.get('/api/movies', (req, res) => {
-
     // connection à la base de données, et sélection des employés
     connection.query('SELECT * from movie', (err, results) => {
-  
-    if (err) {
-        // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
-        res.status(500).send('Erreur lors de la récupération des films');
-
-    } else {
-        // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
-        res.json(results);
+        if (err) {
+            // Si une erreur est survenue, alors on informe l'utilisateur de l'erreur
+            res.status(500).send('Erreur lors de la récupération des films');
+        } else {
+            // Si tout s'est bien passé, on envoie le résultat de la requête SQL en tant que JSON.
+            res.json(results);
         }
-
     });
 });
 
 app.get('/api/movies/names', (req, res) => {
-
     connection.query('SELECT name from movie', (err, results) =>{
-
         if(err) {
             res.status(500).send('No body was here..')
         } else {
@@ -39,7 +33,6 @@ app.get('/api/movies/names', (req, res) => {
 app.listen(port, (err) => {
     if (err) {
         throw new Error('Something bad happened...');
-}
-
+    }
     console.log(`Server is listening on ${port}`);
 });
